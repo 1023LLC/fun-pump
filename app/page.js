7d45f99d1faef7f9d3e53810640c9758800d59b3
julius <nyerereoffice@gmail.com>
2025-04-26 15:50:36 +0300

    feat(home): fetch and display token sales from blockchain

--- app/page.js
+++ app/page.js
@@ -19,6 +19,7 @@ export default function Home() {
   const [account, setAccount] = useState(null)
   const [factory, setFactory] = useState(null)
   const [fee, setFee] = useState(0)
+  const [tokens, setTokens] = useState([])
   const [showCreate, setShowCreate] = useState(false)
 
 
@@ -38,6 +39,25 @@ export default function Home() {
     const fee = await factory.fee()
     setFee(fee)
 
+    const totalTokens = await factory.totalTokens()
+    const tokens = []
+
+    for (let i = 0; i < totalTokens; i++) {
+      const tokenSale = await factory.getTokenSale(i)
+      const token = {
+        token: tokenSale.token,
+        name: tokenSale.name,
+        creator: tokenSale.creator,
+        sold: tokenSale.sold,
+        raised: tokenSale.raised,
+        isOpen: tokenSale.isOpen,
+        image: images[i]
+      }
+
+      tokens.push(token)
+    }
+
+    setTokens(tokens.reverse())
   }
 
   useEffect(() => {
